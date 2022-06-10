38849ec57d75e47fe15e99ee2f7e7e3a8d060e0d
Matúš Holotňák <matus.holotnak@gmail.com>
2022-06-10 12:40:54 +0200

    passport serialize, deserialize

--- app.js
+++ app.js
@@ -26,15 +26,23 @@ const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 passport.use(
   new GitHubStrategy(
     {
-      clientID: env.GITHUB_CLIENT_ID,
-      clientSecret: env.GITHUB_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/github/callback",
+      clientID: GITHUB_CLIENT_ID,
+      clientSecret: GITHUB_CLIENT_SECRET,
+      callbackURL: `http://localhost:${PORT}/auth/github/callback`,
     },
     function (accessToken, refreshToken, profile, done) {
       return done(null, profile);
     }
   )
 );
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
 
 /*
  *  Express Project Setup
