b7f02cb114c409988170c0a0f6ed1619f5a4788c
Matúš Holotňák <matus.holotnak@gmail.com>
2022-06-10 12:57:40 +0200

    fix ensureAuthenticated

--- app.js
+++ app.js
@@ -103,9 +103,9 @@ app.listen(PORT, () => console.log(`Listening on ${PORT}`));
  * ensureAuthenticated Callback Function
  */
 
-const ensureAuthenticated = (req, res, next) => {
+function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
   res.redirect("/login");
-};
+}
