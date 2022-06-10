fb97c95306d0ed940cce0fb1b4bb31c61db4398b
Matúš Holotňák <matus.holotnak@gmail.com>
2022-06-10 12:36:04 +0200

    setup passport

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
  * Package Imports
  */
 const session = require("express-session");
+const passport = require("passport");
+const GitHubStrategy = require("passport-github2").Strategy;
 
 const path = require("path");
 require("dotenv").config();
@@ -21,6 +23,18 @@ const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 /*
  * Passport Configurations
  */
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/github/callback",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      return done(null, profile);
+    }
+  )
+);
 
 /*
  *  Express Project Setup
@@ -32,6 +46,8 @@ app.use(
     saveUninitialized: false,
   })
 );
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
